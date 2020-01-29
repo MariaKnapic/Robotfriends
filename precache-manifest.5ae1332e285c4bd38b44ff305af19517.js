fbafb5696a2292690d83a17753aff00870d2c761
unknown <maria_knapic_afonso@hotmail.com>
2020-01-29 13:22:49 +0000

    Updates

---
+++ precache-manifest.5ae1332e285c4bd38b44ff305af19517.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9a020ac118d4b356ccd296cc429b0ce",
+    "url": "/index.html"
+  },
+  {
+    "revision": "037c229f7ba2ec85306a",
+    "url": "/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "83f9e9142b9de34992a1",
+    "url": "/static/css/main.98eeac59.chunk.css"
+  },
+  {
+    "revision": "037c229f7ba2ec85306a",
+    "url": "/static/js/2.75d9c582.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.75d9c582.chunk.js.LICENSE"
+  },
+  {
+    "revision": "83f9e9142b9de34992a1",
+    "url": "/static/js/main.f220c4a5.chunk.js"
+  },
+  {
+    "revision": "97132822d4a64e50d487",
+    "url": "/static/js/runtime-main.3a4f040e.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
